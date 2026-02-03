696de17a3d453e29be9945aae66564e294567636
kelly6226 <kelly6226@naver.com>
2026-02-03 22:56:32 +0900

    feat(low): font 사용 예시 추가

--- src/app/workflow/page.tsx
+++ src/app/workflow/page.tsx
@@ -5,13 +5,17 @@ import React from "react";
 export default function WorkflowPage() {
   // styled system 사용 방식 예시
   return (
-    <CenterColumn width="100%" height="1500px" bg="gray_500">
-      <Font typo="hero" color="blue_500">
+    <CenterColumn width="100%" height="1500px" bg="blue_300">
+      <Font typo="hero" color="blue_800">
         Font 컴포넌트 사용하는 방식 예시입니다.
       </Font>
 
-      <Row p="20px 10px 20px 10px" bg="white">
-        <Column width="100px" height="100px" bg="red_500"></Column>
+      <Row p="20px 10px 20px 10px" bg="alpha_light_10">
+        <CenterColumn width="auto" height="100px" bg="red_50" px="20px">
+          <Font typo="headline_s" color="red_600">
+            Font 컴포넌트 사용하는 방식 예시입니다.
+          </Font>
+        </CenterColumn>
       </Row>
     </CenterColumn>
   );
