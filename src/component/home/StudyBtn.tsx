b3299846ef4e09a7bca98919ec5b4d856410cac0
kelly6226 <kelly6226@naver.com>
2026-02-06 21:34:51 +0900

    fix(low): btn 깨짐 이슈 수정

--- src/component/home/StudyBtn.tsx
+++ src/component/home/StudyBtn.tsx
@@ -9,7 +9,7 @@ export default function StudyBtn() {
       bg="blue_700"
       borderRadius="8px"
       gridGap="4px"
-      width="104px"
+      width="auto"
       height="40px"
       style={{
         cursor: "pointer",
